c10f35d46081ce955fbb4deae90a5344c3c574c3
DreaSmizz <a.smith102781@gmail.com>
2023-02-23 22:21:46 +0000

    Updated backend

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -11,6 +11,16 @@ class NotificationsActivities:
       'likes_count': 5,
       'replies_count': 1,
       'reposts_count': 0,
+      'replies': [{
+        'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
+        'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+        'handle': 'worf',
+        'message': 'this post has no honor!',
+        'likes_count': 0,
+        'replies_count': 0,
+        'reposts_count': 0,
+        'created_at': (now - timedelta(days=2)).isoformat()
+      }],
     }
     ]
     return results
