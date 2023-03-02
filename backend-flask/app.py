4285c20a7520eb93d453a77bcbb6e78be321b917
DreaSmizz <a.smith102781@gmail.com>
2023-03-02 21:42:24 +0000

    Updated

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -32,8 +32,8 @@ from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProces
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
-#trace.set_tracer_provider(provider)
-#tracer = trace.get_tracer(__name__)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
 
 # X-RAY ------
 xray_url = os.getenv("AWS_XRAY_URL")
