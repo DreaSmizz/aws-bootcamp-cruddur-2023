5de163b29b62e1049698c75d90c7f7bed8a0d539
DreaSmizz <a.smith102781@gmail.com>
2023-03-03 21:04:17 +0000

    add Rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -44,11 +44,35 @@ app = Flask(__name__)
 # X-RAY -------
 XRayMiddleware(app, xray_recorder)
 
+# Rollbar -------
+from time import strftime
+import os
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 # HoneyComb
 # Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 
+# Rollbar --------
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
@@ -81,6 +105,11 @@ def data_messages(handle):
   else:
     return model['data'], 200
   return
+  
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/messages", methods=['POST','OPTIONS'])
 @cross_origin()
