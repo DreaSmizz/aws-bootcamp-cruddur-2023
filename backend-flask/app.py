771c1e2cafa9ab15a165d104f6e707a2adac89ad
DreaSmizz <a.smith102781@gmail.com>
2023-03-10 03:33:29 +0000

    Rollbar update

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -51,13 +51,13 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 # X-RAY ------
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+#xray_url = os.getenv("AWS_XRAY_URL")
+#xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
 app = Flask(__name__)
 
 # X-RAY -------
-XRayMiddleware(app, xray_recorder)
+#XRayMiddleware(app, xray_recorder)
 
 # Rollbar -------
 from time import strftime
@@ -148,7 +148,7 @@ def data_create_message():
   return
 
 @app.route("/api/activities/home", methods=['GET'])
-@xray_recorder.capture('activities_home')
+#@xray_recorder.capture('activities_home')
 def data_home():
   data = HomeActivities.run()
   return data, 200
@@ -159,7 +159,7 @@ def data_notifications():
   return data, 200
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
-@xray_recorder.capture(('activities_users'))
+#@xray_recorder.capture(('activities_users'))
 def data_handle(handle):
   model = UserActivities.run(handle)
   if model['errors'] is not None:
@@ -191,7 +191,7 @@ def data_activities():
   return
 
 @app.route("/api/activities/<string:activity_uuid>", methods=['GET'])
-@xray_recorder.capture(('activities_show'))
+#@xray_recorder.capture(('activities_show'))
 def data_show_activity(activity_uuid):
   data = ShowActivity.run(activity_uuid=activity_uuid)
   return data, 200
