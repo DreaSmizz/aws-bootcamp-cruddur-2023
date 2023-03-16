8e1fbc5932faad07ba25c86740edd7dc94f69912
DreaSmizz <a.smith102781@gmail.com>
2023-03-16 00:15:35 +0000

    Updating code

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -37,7 +37,7 @@ export default function HomeFeedPage() {
     }
   };
 
-  const checkAuth = async () => {
+const checkAuth = async () => {
     Auth.currentAuthenticatedUser({
       // Optional, By default is false. 
       // If set to true, this call will send a 
