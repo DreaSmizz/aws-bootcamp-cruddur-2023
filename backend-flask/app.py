61cbcb86aa81af5c7d74a3ad70ba840e5d279d16
DreaSmizz <a.smith102781@gmail.com>
2023-03-21 02:05:55 +0000

    jwt final token update

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -15,7 +15,7 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
-from lib.cognito_jwt_token import CognitoJwtToken
+from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError
 
 # X-RAY -----
 #from aws_xray_sdk.core import xray_recorder
@@ -160,18 +160,17 @@ def data_create_message():
 #@xray_recorder.capture('activities_home')
 #@aws_auth.authentication_required
 def data_home():
-  access_token = CognitoJwtToken.extract_access_token(request.headers)
+  access_token = extract_access_token(request.headers)
   try:
-    claims = cognito_jwt_token.token_service.verify(access_token)
+    claims = cognito_jwt_token.verify(access_token)
+    app.logger.debug("authenticated")
+    app.logger.debug(claims)
+    app.logger.debug(claims['username'])
+    data = HomeActivities.run(cognito_user_id=claims['username'] )
   except TokenVerifyError as e:
-     _ = request.data
-     abort(make_response(jsonify(message=str(e)), 401))
-
-  app.logger.debug('claims')
-  app.logger.debug(claims)
-  data = HomeActivities.run()
-  claims = aws_auth.claims
-
+    app.logger.debug(e)
+    app.logger.debug("unauthenticated")
+    data = HomeActivities.run()
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
